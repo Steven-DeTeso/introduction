90b342b0f1e9bcd4f0c73e9685bdd948a00ea5a4
Steven DeTeso <sadeteso@gmail.com>
2022-09-28 10:20:34 -0400

    further testing code

--- testing_python.py
+++ testing_python.py
@@ -36,13 +36,41 @@
 # Utilize a method from the documentation and print the result.
 
 
-for x in range(0, 10, 2):
-    print(x)
-# output: 0, 2, 4, 6, 8
-for x in range(5, 1, -3):
-    print(x)
-# output: 5, 2
+# for x in range(0, 10, 2):
+#     print(x)
+# # output: 0, 2, 4, 6, 8
+# for x in range(5, 1, -3):
+#     print(x)
+# # output: 5, 2
 
-# Challenge: Write a for loop to print all integers from 1 to 20, including 20
-for x in range(1, 21):
-    print(x)
+# # Challenge: Write a for loop to print all integers from 1 to 20, including 20
+# for x in range(1, 21):
+#     print(x)
+
+# countries = ["Uganda", "Chile", "Albania", "Saudi Arabia"]
+
+# Challenge 1: Fix the range!
+# for integer in range(0, len(countries)):
+    # print(countries[integer])
+    # Challenge 2: print the index here
+    # print(countries)
+    # Challenge 3: print the country here
+
+# Looping over values only...
+# for country in countries:
+#     print(country)
+    # Challenge 4: print the country here
+
+# count = 0
+# while count <= 5:
+#     print("looping - ", count)
+#     count += 1
+
+y = 3
+while y > 0:
+    print(y)
+    y = y - 1
+    if y == 0:
+        break
+else:		# only executes on a clean exit from the while loop (i.e. not a break)
+    print("Final else statement")
